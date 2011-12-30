c5d1776b50938cc0946f901a8fb65b21a5c5fbaa
liwh <liwh87@gmail.com>
2011-12-30 20:33:35 +0800

    fixed: the operation not effect last operation symbol

--- Caculator/CalculatorBrain.m
+++ Caculator/CalculatorBrain.m
@@ -10,28 +10,41 @@
 
 @interface CalculatorBrain()
 @property(nonatomic,strong)NSMutableArray *operandStack;
+@property(nonatomic,strong)NSString *operationSymbol;
 @end
 @implementation CalculatorBrain
 @synthesize operandStack = _operandStack;
+@synthesize operationSymbol = _operationSymbol;
 -(NSMutableArray *)operandStack{
     if (_operandStack == nil){
         _operandStack = [[NSMutableArray alloc] init];
     }
     return _operandStack;
 }
+-(NSString *)operationSymbol{
+    if (_operationSymbol == nil){
+        _operationSymbol = [[NSString alloc] init];
+    }
+    return _operationSymbol;
+}
 -(void) pushOperand:(double)operand 
 {
     [self.operandStack addObject:[NSNumber numberWithDouble:operand]];
+}
+-(void) pushOperation:(NSString *)operation
+{
+    self.operationSymbol = operation;
 }
 -(double) popOperand {
     NSNumber *operanObject = [self.operandStack lastObject];
     if(operanObject) [self.operandStack removeLastObject];
     return [operanObject doubleValue];
 }
-- (double)performOperation:(NSString *)operation
+- (double)performOperation:(NSString *)operationString
 {
     double result = 0;
-    //caculate the result
+    //caculate the result2
+    NSString *operation = [self operationSymbol];
     if ([operation isEqualToString:@"+"]){
         result = [self popOperand] + [self popOperand];
     }else if([@"*" isEqualToString:operation]){
@@ -44,8 +57,11 @@
         double a = [self popOperand];
         double b = [self popOperand];
         result = b ? b - a : a;
+    }else{
+        result = [self popOperand];
     }
     [self pushOperand:result];
+    [self pushOperation:operationString];
     return result;
 }
 @end
